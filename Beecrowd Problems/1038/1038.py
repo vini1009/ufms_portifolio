f2dc9fa1f17e9857c5115ee7545520cf3273e113
vini1009 <viniciuss2616@gmail.com>
2025-04-03 22:50:25 -0400

    Correções 1038

--- Beecrowd Problems/1038/1038.py
+++ Beecrowd Problems/1038/1038.py
@@ -1,1 +1,30 @@
+# prices = {"Cachorro Quente": 4.00, "X-Salada": 4.50, "X-Bacon": 5.00, "Torrada Simples": 2.00, "Refrigerante": 1.50}
 
+choice_1, choice_2 = map(int, input().split()) # Obtem a entrada de pedidos. Ex.:3 2
+
+products = {
+    1: {
+        "name": "Cachorro Quente",
+        "price": 4.00 
+    },
+    2: {
+        "name": "X-Salada",
+        "price": 4.50
+    },
+    3: {
+        "name": "X-Bacon",
+        "price": 5.00
+    },
+    4: {
+        "name": "Torrada Simples",
+        "price": 2.00
+    },
+    5: {
+        "name": "Refrigerante",
+        "price": 1.50
+    }
+}
+
+result = products[choice_1]['price'] * choice_2 # Multiplica o Preço pela Quantia
+
+print(f"Total: R$ {result:.2f}")
