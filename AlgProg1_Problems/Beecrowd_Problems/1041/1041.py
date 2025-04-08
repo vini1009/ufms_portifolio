422cb51880c044175617176ca07ec102ccfd410b
vini1009 <viniciuss2616@gmail.com>
2025-04-07 21:10:21 -0400

    Fix 1041

--- AlgProg1_Problems/Beecrowd_Problems/1041/1041.py
+++ AlgProg1_Problems/Beecrowd_Problems/1041/1041.py
@@ -7,10 +7,10 @@ elif (x < 0.0 and y > 0.0):
 elif (x < 0.0 and y < 0.0):
     print("Q3")
 elif (x > 0.0 and y < 0.0):
-    print("Q3")
+    print("Q4")
 elif (x != 0.0 and y == 0.0):
-    print("Eixo Y")
-elif (x == 0.0 and y != 0.0):
     print("Eixo X")
+elif (x == 0.0 and y != 0.0):
+    print("Eixo Y")
 else:
     print("Origem")
