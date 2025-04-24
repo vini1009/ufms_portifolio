bdb06ffa48798464cc9d1051c03265b12474a702
vini1009 <viniciuss2616@gmail.com>
2025-04-23 21:01:34 -0400

    1042.py fix

--- AlgProg1_Problems/Beecrowd_Problems/1042/1042.py
+++ AlgProg1_Problems/Beecrowd_Problems/1042/1042.py
@@ -1,8 +1,41 @@
-# Resolução | 1042
+a, b, c = map(int, input().split())
 
-i_1, i_2, i_3 = map(int, input().split())
+x = a
+y = b
+z = c
 
+if a <= b and a <= c:
+    if b <= c:
+        first = a
+        second = b
+        third = c
+    else:
+        first = a
+        second = c
+        third = b
+elif b <= a and b <= c:
+    if a <= c:
+        first = b
+        second = a
+        third = c
+    else:
+        first = b
+        second = c
+        third = a
+else:
+    if a <= b:
+        first = c
+        second = a
+        third = b
+    else:
+        first = c
+        second = b
+        third = a
 
-
-if (i_1 < i_2):
-    
+print(first)
+print(second)
+print(third)
+print()
+print(x)
+print(y)
+print(z)
